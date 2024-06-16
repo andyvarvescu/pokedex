e312ea6d5bafc3f0716d7be86f80366061da3aea
Andy Varvescu <andi.varvescu@gmail.com>
2024-06-16 21:33:12 +0300

    add title

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import Typography from "@mui/material/Typography";
+import "@fontsource/roboto/400.css";
 
 export default function Home() {
   return (
-    <div></div>
+    <Typography variant="h4" gutterBottom>
+      Pokedex
+    </Typography>
   );
 }
