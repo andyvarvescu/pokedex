5cd2c30088c051844cf9b2c7877313cf6bb74b57
Andy Varvescu <andi.varvescu@gmail.com>
2024-06-16 21:44:14 +0300

    add name input

--- app/page.js
+++ app/page.js
@@ -1,10 +1,26 @@
-import Typography from "@mui/material/Typography";
+"use client";
 import "@fontsource/roboto/400.css";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
 
 export default function Home() {
   return (
-    <Typography variant="h4" gutterBottom>
-      Pokedex
-    </Typography>
+    <>
+      <Typography variant="h4" gutterBottom>
+        Pokedex
+      </Typography>
+
+      <Stack direction="row" spacing={1}>
+        <TextField
+          id="outlined-basic"
+          label="filter by name input"
+          variant="outlined"
+          size="small"
+          value={"test"}
+          onChange={(e) => {}}
+        />
+      </Stack>
+    </>
   );
 }
