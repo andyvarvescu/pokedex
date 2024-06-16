c161c7a20906b0f32ffbf4323777b0d6425cea37
Andy Varvescu <andi.varvescu@gmail.com>
2024-06-16 21:59:39 +0300

    add Type dropdown

--- app/page.js
+++ app/page.js
@@ -1,25 +1,53 @@
 "use client";
+import { useState } from "react";
 import "@fontsource/roboto/400.css";
+import FormControl from "@mui/material/FormControl";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
+import Select from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
 
 export default function Home() {
+  const [filterType, setFilterType] = useState("");
+
+  const handleTypeChange = (event) => {
+    setFilterType(event.target.value);
+  };
+
   return (
     <>
       <Typography variant="h4" gutterBottom>
         Pokedex
       </Typography>
 
-      <Stack direction="row" spacing={1}>
+      <Stack direction="row" spacing={3}>
         <TextField
           id="outlined-basic"
-          label="filter by name input"
+          label="Filter by Name"
           variant="outlined"
           size="small"
           value={"test"}
           onChange={(e) => {}}
         />
+        <FormControl sx={{ m: 1, minWidth: 200 }} size="small">
+          <InputLabel id="filter-by-type-label">Filter by Type</InputLabel>
+          <Select
+            labelId="filter-by-type-label"
+            id="filter-by-type"
+            value={filterType}
+            label="Select Type"
+            onChange={handleTypeChange}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
       </Stack>
     </>
   );
